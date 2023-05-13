40dcf479a127818121eb65450e929d4e874ae946
Marwan MaNo <marwanhassan954@gmail.com>
2023-05-13 14:49:25 +0300

    .

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ const rateSubmit = document.querySelector('.submit-rating');
 const ratingSection = document.querySelector('.rating-section');
 const thanksSection = document.querySelector('.thanks-section')
 // loop all rating buttons
-ratings.forEach(rate=>{
+ratings.forEach(rate => {
     // listen to the selected button
-    rate.addEventListener('click', (ratingSelected)=>{
+    rate.addEventListener('click', (ratingSelected) => {
         // storing value of rating selection
         ratingSelected = rate.innerHTML;
         // console.log(ratingSelected); just for try and error 
-        rateSubmit.addEventListener('click', ()=>{
+        rateSubmit.addEventListener('click', () => {
             // hide rating section and review thanks section
             ratingSection.classList.add('d-none');
             thanksSection.classList.remove('d-none');
@@ -18,9 +18,9 @@ ratings.forEach(rate=>{
             document.querySelector('#rating-result').innerHTML = ratingSelected;
         })
     })
-    
+
 })
-    console.log(rateReview);
+
 
 
 
